5d987c0a8e14ede6e88834391f2a128dca784217
Nik Shikov <nik.shikov@gmail.com>
2018-07-19 10:46:02 +0300

    Add NodeJs EventEmitter usage exmaple

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # event-emmiter
 
-This is just an simple implementation of NodeJS EventEmitter with usage example
+This is just an simple implementation EventEmitter. 
+app.js contains example of simple EventEmitter usage and example of NodeJs EventEmitter usage
 
 To run example with node 10:
 ```shell
--- src/app.mjs
+++ src/app.mjs
@@ -1,4 +1,9 @@
 import Emitter from './emitter.mjs';
+import EventEmitter from 'events';
+import util from 'util';
+
+// Simple EventEmitter usage
+console.log('--=== Simple EventEmitter example ===--')
 let emitter = new Emitter();
 
 emitter.on('greet', function () {
@@ -11,3 +16,23 @@ emitter.on('greet', function () {
 
 console.log('Hello!');
 emitter.emit('greet');
+
+// NodeJS EventEmitter usage
+console.log('--=== NodeJS EventEmitter example ===--')
+function Greetr() {
+    this.greeting = 'Hello world!';
+}
+
+util.inherits(Greetr, EventEmitter);
+Greetr.prototype.greet = function () {
+    console.log(this.greeting);
+    this.emit('greet');
+}
+
+var greeter1 = new Greetr();
+
+greeter1.on('greet', function () {
+    console.log('Someone greeted!')
+});
+
+greeter1.greet();
